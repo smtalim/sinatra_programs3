6a7cc080acc9088a6356e1a7eac03d10da4f6745
SatishTalim <satish@rubylearning.org>
2013-03-14 06:56:38 +0530

    commit

--- myapp5.rb
+++ myapp5.rb
@@ -5,9 +5,16 @@ get '/' do
   erb :index
 end
 
-# When a Sinatra::NotFound exception is raised,
-# or the response’s status code is 404, 
-# the not_found handler is invoked:
+get '/div_by_zero' do
+  0 / 0
+  "You won't see me."
+end
+
 not_found do
   erb :'404'
 end
+
+error do
+  erb :'500'
+end
+
